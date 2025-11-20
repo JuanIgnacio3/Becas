9f45f7d5c755165f702d7919ddc685a059e50b99
Juan Ignacio Vargas Ramírez <jvargas0312@ufide.ac.cr>
2025-11-19 20:31:46 -0600

    implementar el identity en el program.cs

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options =>
@@ -14,53 +13,54 @@ builder.Services.AddSession(options =>
 });
 builder.Services.AddHttpContextAccessor();
 
-// Repositorio en memoria respaldado por Session
-builder.Services.AddScoped<Becas.Data.ISolicitudBecaRepository, Becas.Data.SessionSolicitudBecaRepository>();
-
-builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
-{
-    options.Password.RequireDigit = false;
-    options.Password.RequireLowercase = false;
-    options.Password.RequireUppercase = false;
-    options.Password.RequireNonAlphanumeric = false;
-    options.Password.RequiredLength = 6;
-})
-.AddDefaultTokenProviders();
-
+// ---- Identity 100% en memoria ----
 builder.Services.AddSingleton<InMemoryUserStore>();
 builder.Services.AddSingleton<InMemoryRoleStore>();
 builder.Services.AddSingleton<IUserStore<IdentityUser>>(sp => sp.GetRequiredService<InMemoryUserStore>());
 builder.Services.AddSingleton<IRoleStore<IdentityRole>>(sp => sp.GetRequiredService<InMemoryRoleStore>());
 
+builder.Services
+    .AddIdentity<IdentityUser, IdentityRole>(options =>
+    {
+        options.Password.RequireDigit = false;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequiredLength = 4;
+    })
+    .AddDefaultTokenProviders();
+
+// Cookies
 builder.Services.ConfigureApplicationCookie(options =>
 {
     options.LoginPath = "/Account/Login";
     options.AccessDeniedPath = "/Account/AccessDenied";
 });
 
+// Seeder
 builder.Services.AddScoped<DummyIdentitySeeder>();
 
 var app = builder.Build();
 
+// Ejecutar seeder (usa tu InMemoryUserStore)
 using (var scope = app.Services.CreateScope())
 {
     var seeder = scope.ServiceProvider.GetRequiredService<DummyIdentitySeeder>();
     await seeder.SeedAsync();
 }
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
 app.UseStaticFiles();
 
 app.UseRouting();
-
 app.UseSession();
+
 app.UseAuthentication();
 app.UseAuthorization();
-
 
 app.MapControllerRoute(
     name: "Becas",
